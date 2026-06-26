7e6b97217cfbad00a3b0342838e7b7ad19cc145b
Venkat <psai96526@gmail.com>
2026-06-26 20:06:22 +0530

    up

---
+++ varaible.py
@@ -0,0 +1,21 @@
+my_name = "venkatasai" ## this is inline comment
+print(my_name)
+## this is comment
+num_servers = 10
+print(num_servers)
+print(type(num_servers))
+
+Const_pi = 3.14 
+print(type(Const_pi))
+
+## this is multiline sting or block comment
+"""
+1. integer or number
+2. float
+3. string
+4. boolean
+5. list 
+6. tuple
+7. dictionart
+8. sets
+"""
